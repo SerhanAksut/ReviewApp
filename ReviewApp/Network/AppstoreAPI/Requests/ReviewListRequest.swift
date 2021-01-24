beedb438c6ae1f1d02347b0f5dfe9cfa83c899f0
Serhan Aksüt <serhanaksut@gmail.com>
2021-01-24 16:51:00 +0300

    review url changed.

--- ReviewApp/Network/AppstoreAPI/Requests/ReviewListRequest.swift
+++ ReviewApp/Network/AppstoreAPI/Requests/ReviewListRequest.swift
@@ -2,7 +2,7 @@
 extension Request where Response == Feed {
     static var reviewList: Request<Feed> {
         Request(
-            path: "nl/rss/customerreviews/id=474495017/sortby=mostrecent/json",
+            path: "/tr/rss/customerreviews/id=375380948/sortby=mostrecent/json",
             httpMethod: .get,
             decoder: decode
         )
